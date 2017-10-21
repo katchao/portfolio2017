d0b8145204f61bbe2a84ac1ade1cc0177106fda8
Katherine Chao <kchao1994@gmail.com>
2017-10-20 23:35:28 -0700

    intro finished

--- scripts/main.js
+++ scripts/main.js
@@ -3,16 +3,49 @@ var contentShowing = false;
 
 function flipView() {
 	if(!contentShowing) {
-		$('.content-wrapper').show();
-		$('.intro-wrapper').hide();
-		contentShowing = true;
+		$('body').fadeOut('slow', function() {
+			$('body').fadeIn();
+			$('.content-wrapper').show();
+			$('header').removeClass('intro-header').addClass('content-header'); // swap headers
+
+			// animated background maintenance
+			clearInterval(bgInterval); // stop animated bg timer
+			$('.bg').css('opacity', '0');
+
+
+
+			contentShowing = true;
+		});
+
 	}
 
-	else {
+	/*else {
 		$('.content-wrapper').hide();
-		$('.intro-wrapper').show();
+		$('header').removeClass('content-header').addClass('intro-header');
 		contentShowing = false;
-	}
+	}*/
+}
+
+function newGradient() {
+	var c1 = {
+		r: Math.floor(Math.random()*255),
+		g: Math.floor(Math.random()*255),
+		b: Math.floor(Math.random()*255)
+	};
+	var c2 = {
+		r: Math.floor(Math.random()*255),
+		g: Math.floor(Math.random()*255),
+		b: Math.floor(Math.random()*255)
+	};
+
+	c1.rgb = 'rgba('+c1.r+','+c1.g+','+c1.b+', 0.8)';
+	c2.rgb = 'rgba('+c2.r+','+c2.g+','+c2.b+', 0.8)';
+	return 'radial-gradient(at top left, '+c1.rgb+', '+c2.rgb+')';
+}
+
+function rollBg() {
+	$('.bg.hidden').css('background', newGradient());
+	$('.bg').toggleClass('hidden');
 }
 
 
@@ -25,17 +58,27 @@ $(function(){
 	$('.header-placeholder').html(headerTemplateComp);
 
 
-	$('.about-me-link').click(function() {
+	$('.nav-link').click(function() {
 		flipView();
 	});
 
 
 
 
-	// test code
-	$('.content-wrapper').show();
-	$('.intro-wrapper').hide();
 
+
+	// test code
+	$('.content-wrapper').hide();
+	$('header').addClass('intro-header');
+
+	rollBg();
+	bgInterval = setInterval(rollBg, 5000);
 
 });
 
+
+
+
+/* credits:
+	animated background: http://www.jqueryscript.net/animation/Creating-Animated-Background-with-Random-Gradient-Transitions-using-jQuery.html
+*/
