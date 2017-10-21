1fd3259c53e38e4685d80b5ebf5ea7de35b0d33f
Katherine Chao <kchao1994@gmail.com>
2017-10-20 20:01:32 -0700

    added handlebars

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,38 @@
 
+var contentShowing = false;
 
-$(document).ready(function(){
+function flipView() {
+	if(!contentShowing) {
+		$('.content-wrapper').show();
+		$('.intro-wrapper').hide();
+		contentShowing = true;
+	}
+
+	else {
+		$('.content-wrapper').hide();
+		$('.intro-wrapper').show();
+		contentShowing = false;
+	}
+}
+
+
+$(function(){
+
+
+	// Handlebars template for header
+	var headerTemplate = Handlebars.compile($("#header-template").html());
+	var headerTemplateComp = headerTemplate();
+	$('.header-placeholder').html(headerTemplateComp);
+
+
+
+
+	$('.content-wrapper').show();
+	$('.intro-wrapper').hide();
+
+	$('.about-me-link').click(function() {
+		flipView();
+	});
 
 
 });
